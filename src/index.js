6e97a6e2c6d76e284170672f8a9d2386b0703fb6
Artem <artyom.balamutyuk@mail.ru>
2019-01-08 11:37:05 +0300

    Add a simple webpack

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./default.css";
+
+const Index = () => {
+  return <div>Hello World!</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
