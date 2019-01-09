65d14769f732b2a29ed585635922c161095eaab5
Artem <artyom.balamutyuk@mail.ru>
2019-01-09 15:19:03 +0300

    Add plugins and file loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,42 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const StatsPlugin = require("stats-webpack-plugin");
+const FriendlyErrorsWebpackPlugin = require("friendly-errors-webpack-plugin");
+const StatsWebpackPlugin = require("stats-webpack-plugin");
+const DuplicatePackageCheckerWebpackPlugin = require("duplicate-package-checker-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 const path = require("path");
 
 const defaultPort = 3000;
 
 const htmlPlugin = new HtmlWebPackPlugin({
+  title: "Development",
   filename: "index.html",
-  template: "./public/index.html"
+  template: "./public/index.html",
+  favicon: "./src/assets/images/favicon.ico"
 });
 
-const statsPlugin = new StatsPlugin("stats.json", {
+const errorPlugin = new FriendlyErrorsWebpackPlugin({
+  compilationSuccessInfo: {
+    messages: [
+      `You application is running on port ${process.env.PORT || defaultPort}`
+    ],
+    notes: ["Yeee compiled successfully"]
+  }
+});
+
+const statsPlugin = new StatsWebpackPlugin("stats.json", {
   chunkModules: true,
   exclude: [/node_modules[\\\/]react/]
 });
+
+const packageChecker = new DuplicatePackageCheckerWebpackPlugin();
+
+const terser = new TerserPlugin({
+  cache: true,
+  parallel: true,
+  sourceMap: true
+});
+
+const hMR = new webpack.HotModuleReplacementPlugin();
 
 module.exports = {
   entry: "./src/index",
@@ -20,6 +44,7 @@ module.exports = {
     path: path.resolve(__dirname, "build"),
     filename: "webpack.bundled.js"
   },
+  devtool: "inline-source-map",
   module: {
     rules: [
       {
@@ -40,8 +65,27 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"]
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"]
       }
     ]
+  },
+  optimization: {
+    minimizer: [terser]
   },
   resolve: {
     alias: {
@@ -53,6 +97,6 @@ module.exports = {
     port: process.env.PORT || defaultPort,
     quiet: true
   },
-  plugins: [htmlPlugin, statsPlugin],
+  plugins: [errorPlugin, htmlPlugin, statsPlugin, packageChecker, hMR],
   profile: true
 };
