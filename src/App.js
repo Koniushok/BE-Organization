79068901d4eacc114c72dcd7e726b766ebf6eb3b
Artem <artyom.balamutyuk@mail.ru>
2019-01-10 17:44:42 +0300

    Replace old HMR with new one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React, { Component, Fragment } from "react";
-import { hot } from "react-hot-loader/root";
+import img1 from "./assets/images/68493.svg";
+import img2 from "./assets/images/coniglio_rabbit_small.svg";
+import img3 from "./assets/images/beauty-bloom-blue-67636.jpg";
+import img4 from "./assets/images/Services2.jpg";
 
 class App extends Component {
   render() {
-    return <Fragment>Art</Fragment>;
+    return (
+      <Fragment>
+        Art
+        <img src={img1} />
+        <img src={img2} />
+        <img src={img3} />
+        <img src={img4} />
+      </Fragment>
+    );
   }
 }
 
-export default hot(App);
+export default App;
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,5 +1,5 @@
 const merge = require("webpack-merge");
-
+const { HotModuleReplacementPlugin } = require("webpack");
 const common = require("./webpack.config.js");
 
 const defaultPort = 3000;
@@ -7,8 +7,11 @@ const defaultPort = 3000;
 module.exports = merge(common, {
   devtool: "inline-source-map",
   mode: "development",
+
+  plugins: [new HotModuleReplacementPlugin()],
   devServer: {
     port: process.env.PORT || defaultPort,
-    quiet: true
+    quiet: true,
+    hot: true
   }
 });
