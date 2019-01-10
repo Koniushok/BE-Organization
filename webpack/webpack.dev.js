d1e1e1fbd465947d2c4c1fdb6cac15c605693c9a
Artem <artyom.balamutyuk@mail.ru>
2019-01-10 18:25:32 +0300

    Add eslint

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,13 +1,22 @@
-const merge = require("webpack-merge");
-const { HotModuleReplacementPlugin } = require("webpack");
-const common = require("./webpack.config.js");
+const merge = require('webpack-merge');
+const { HotModuleReplacementPlugin } = require('webpack');
+const common = require('./webpack.config.js');
 
 const defaultPort = 3000;
 
 module.exports = merge(common, {
-  devtool: "inline-source-map",
-  mode: "development",
-
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'eslint-loader'
+      }
+    ]
+  },
   plugins: [new HotModuleReplacementPlugin()],
   devServer: {
     port: process.env.PORT || defaultPort,
